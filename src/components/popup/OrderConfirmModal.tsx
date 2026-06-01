ad6d551ed78d0735d1118e7c5df4b9fc93e5b627
Joseph Nguyen <josephnguyen1906@gmail.com>
2026-06-01 18:40:18 +0700

    update

--- src/components/popup/OrderConfirmModal.tsx
+++ src/components/popup/OrderConfirmModal.tsx
@@ -179,8 +179,8 @@ export default function OrderConfirmModal({
               mb={2}
             >
               {dataOrder.is_win === 1
-                ? `+ ${Number(dataOrder?.ploss).toLocaleString()}`
-                : `- ${Number(dataOrder?.ploss).toLocaleString()}`}
+                ? `+ ${Number(dataOrder?.ploss + dataOrder?.num).toLocaleString()}`
+                : `- ${Number(dataOrder?.num + dataOrder?.ploss).toLocaleString()}`}
             </Typography>
 
             <InfoRow
@@ -210,8 +210,8 @@ export default function OrderConfirmModal({
               label={t("BuySellPage.Profit_loss")}
               value={
                 dataOrder.is_win === 1
-                  ? `+ ${Number(dataOrder?.ploss).toLocaleString()}`
-                  : `- ${Number(dataOrder?.ploss).toLocaleString()}`
+                  ? `+ ${Number(dataOrder?.ploss + dataOrder?.num).toLocaleString()}`
+                  : `- ${Number(dataOrder?.ploss + dataOrder?.num).toLocaleString()}`
               }
               color={dataOrder.is_win === 1 ? "green" : "red"}
             />
