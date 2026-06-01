e34eb456e45eb85bcc8170598c792afc069a3feb
Joseph Nguyen <josephnguyen1906@gmail.com>
2026-06-01 12:32:11 +0700

    update code

--- src/pages/Trade/Trade.page.tsx
+++ src/pages/Trade/Trade.page.tsx
@@ -89,13 +89,13 @@ export default function TradePage() {
   const { t } = useTranslation();
   const { user, fetchUser } = useUserStore();
   const [selectedCoin, setSelectedCoin] = useState<Icoin>({
-    coinname: "btc",
-    id: 1,
-    name: "btcusdt",
+    id: 43,
+    coinname: "xau",
+    name: "xauusdt",
+    symbol: "XAUUSD",
+    title: "XAU/USD",
     sort: 1,
     status: 1,
-    symbol: "btc-usdt",
-    title: "BTC/USDT",
   });
 
   const [tab, setTab] = useState("BUY");
